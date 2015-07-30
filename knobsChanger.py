044e7c4c7e8c22fd29c08134a8f414e7b8408aff
lowerfield@gmail.com <manuelperez@manuels-mbp.config>
2015-07-30 12:43:30 +0100

    add enumeration and pulldown

--- knobsChanger.py
+++ knobsChanger.py
@@ -2,11 +2,16 @@
 par = [] # this will remember the last entry, keep it out of the funcion
 
 class knobsChanger( nukescripts.PythonPanel ):
-    def __init__(self, k, paramName):
+    def __init__(self, knobClass, knobName, knobObject):
         nukescripts.PythonPanel.__init__( self, "test")
 
-        knobT = getattr(nuke,k)
-        self.knob = knobT(paramName)
+        knobT = getattr(nuke,knobClass)
+
+        if knobClass in ('Enumeration_Knob', 'Pulldown_Knob'):
+            enumValues = knobObject.values()
+            self.knob = knobT(knobName, knobName, enumValues)
+        else:
+            self.knob = knobT(knobName)
         self.addKnob(self.knob)
 
     def showModalDialog(self):
@@ -21,29 +26,29 @@ def changer():
         if nodes:  
             # this if statement remembers the last entry, using par variable out of function
             if par != []:
-                param = nuke.getInput('Parameter to change', ' '.join(par))
-                if param == None:
+                knob = nuke.getInput('Parameter to change', ' '.join(par))
+                if knob == None:
                     pass
                 else:
                     par.pop()
-                    par.append(param)
+                    par.append(knob)
             else:
-                param = nuke.getInput('Parameter to change', '')
-                par.append(param)
-                if param == None:
+                knob = nuke.getInput('Parameter to change', '')
+                par.append(knob)
+                if knob == None:
                     par.pop()
             
-        paramObject = nuke.selectedNode().knob('%s' %param)
-        paramName = paramObject.name()
-        paramClass = paramObject.Class()
-        paramValue = paramObject.value()
-        #paramValues = paramObject.values()
+        knobObject = nuke.selectedNode().knob('%s' %knob)
+        knobName = knobObject.name()
+        knobClass = knobObject.Class()
+        knobValue = knobObject.value()
+        #knobValues = knobObject.values()
 
-        result = knobsChanger(paramClass,paramName).showModalDialog()
+        result = knobsChanger(knobClass,knobName,knobObject).showModalDialog()
 
         for node in nodes:
             try:
-                node[paramName].setValue(result)
+                node[knobName].setValue(result)
             except:
                 pass
   
